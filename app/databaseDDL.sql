cefd106480dbe380f700f34ea24fbbdf7a532683
alcereo <ak-91@mail.ru>
2016-10-05 23:03:09 +1000

    Add DDL from postgres

--- app/databaseDDL.sql
+++ app/databaseDDL.sql
@@ -21,3 +21,71 @@ CREATE TABLE public.notes (
   MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 CREATE UNIQUE INDEX notes_id_uindex ON notes USING BTREE (id);
+
+-- Postgres ------------------ ------
+
+-- Table: articles
+
+-- DROP TABLE articles;
+
+CREATE TABLE articles
+(
+  uid uuid NOT NULL DEFAULT uuid_generate_v4(),
+  id serial NOT NULL,
+  title text NOT NULL,
+  contenthtml text NOT NULL,
+  contentmarkdown text NOT NULL,
+  parentuid uuid,
+  noteuid uuid NOT NULL,
+  CONSTRAINT articles_pkey PRIMARY KEY (uid),
+  CONSTRAINT articles_notes_uid_fk FOREIGN KEY (noteuid)
+  REFERENCES notes (uid) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT articles_parentuid_fkey FOREIGN KEY (parentuid)
+  REFERENCES articles (uid) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE articles
+  OWNER TO postgres;
+
+-- Index: articles_id_uindex
+
+-- DROP INDEX articles_id_uindex;
+
+CREATE UNIQUE INDEX articles_id_uindex
+  ON articles
+  USING btree
+  (id);
+
+
+-- Table: notes
+
+-- DROP TABLE notes;
+
+CREATE TABLE notes
+(
+  uid uuid NOT NULL DEFAULT uuid_generate_v4(),
+  mainarticleuid uuid,
+  id serial NOT NULL,
+  CONSTRAINT notes_pkey PRIMARY KEY (uid),
+  CONSTRAINT notes_mainarticleuid_fkey FOREIGN KEY (mainarticleuid)
+  REFERENCES articles (uid) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE notes
+  OWNER TO postgres;
+
+-- Index: notes_id_uindex
+
+-- DROP INDEX notes_id_uindex;
+
+CREATE UNIQUE INDEX notes_id_uindex
+  ON notes
+  USING btree
+  (id);
